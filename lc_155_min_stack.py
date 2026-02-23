8238f02ab9899513de207f78fde17e15b409fd33
MaerusWu <maeruslovesleep@gmail.com>
2026-02-23 23:27:35 +0800

    [Stack] LC-155: Min Stack - O(1) Time / O(N) Space - Two Stacks

---
+++ lc_155_min_stack.py
@@ -0,0 +1,29 @@
+class MinStack:
+    #Base on Neetcode's video explanation
+    def __init__(self):
+        self.stack = []
+        self.minStack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        val = min(val, self.minStack[-1] if self.minStack else val)
+        self.minStack.append(val)
+
+    def pop(self) -> None:
+        self.stack.pop()
+        self.minStack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.minStack[-1]
+
+minStack = MinStack()
+minStack.push(1)
+minStack.push(2)
+minStack.push(0)
+print(minStack.getMin())
+minStack.pop()
+print(minStack.top())
+print(minStack.getMin())
